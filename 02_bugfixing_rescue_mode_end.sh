02d89d49d0a3b51cf13a2c4ad75e707e907f1293
M. Haeckl <maximilian.haeckl@dcso.de>
2018-04-12 06:36:21 +0200

    Update 02_bugfixing_rescue_mode_end.sh

--- 02_bugfixing_rescue_mode_end.sh
+++ 02_bugfixing_rescue_mode_end.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+update-initramfs -u
+update-grub
+exit
+
 umount /mnt/var/ /mnt/backup /mnt/boot /mnt/dev /mnt/sys /mnt/proc
 umount /mnt/
 
 vgchange -a n vg0
 
 cryptsetup luksClose crypt
+
+
+reboot
