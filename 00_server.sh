50c1c2be06a870a7cc3943eabd815f75cb6e1f03
Max H <10329648+8ear@users.noreply.github.com>
2019-05-03 19:06:59 +0200

    Update 00_server.sh

--- 00_server.sh
+++ 00_server.sh
@@ -15,10 +15,11 @@ mount /dev/vg0/root /mnt
 mount /dev/vg0/backup /mnt/backup
 mount /dev/vg0/var /mnt/var
 mount /dev/vg0/log /mnt/var/log
-mount /dev/sda1 /mnt/boot
 mount --bind /dev /mnt/dev
 mount --bind /sys /mnt/sys
 mount --bind /proc /mnt/proc
+mount /dev/sda1 /mnt/boot  # OR for software raid: mount /dev/md0 /mnt/boot
+
 
 # To let the system know there is a new crypto device we need to edit the cryptab(/etc/crypttab):
 echo "crypt /dev/sda2 none luks" >> /mnt/etc/crypttab
@@ -37,14 +38,19 @@ sed -i 's/use_lvmetad = 1/use_lvmetad = 0/g' /etc/lvm/lvm.conf
 echo "now create your ssh key on client"
 echo "ready?"
 read input
-echo "now scp your ssh public key to /root/.ssh/id_rsa.pub"
+
+echo "now copy your hetzner_unlock ssh public key to /etc/dropbear-initramfs/authorized_keys"
 read input
 nano /etc/dropbear-initramfs/authorized_keys
+
+echo "now copy your hetzner_login ssh public key to /root/.ssh/id_rsa.pub"
+read input
+nano /root/authorized_keys
 
 # Regenerate the initramfs:
 update-initramfs -u
 update-grub
-grub-install /dev/sda
+grub-install /dev/sda # and for software raid: grub-install /dev/sdb
 
 # To be sure the network interface is coming up:
 echo "/sbin/ifdown --force enp" >> /etc/rc.local
@@ -61,7 +67,7 @@ umount /mnt/dev
 umount /mnt/sys
 umount /mnt/proc
 
-rsync -a /mnt/ /oldroot/
+rsync -av /mnt/ /oldroot/
 
 umount /mnt/backup
 umount /mnt/var/log
@@ -75,11 +81,11 @@ vgchange -a n
 vgremove vg0
 
 # After this, we encrypt our raid 1 now.
-#- `cryptsetup --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 6000 luksFormat /dev/md1`
+# for software raid: cryptsetup --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 6000 luksFormat /dev/md1 
 cryptsetup --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 6000 luksFormat /dev/sda2
 #(!!!Choose a strong passphrase (something like `pwgen 64 1`)!!!)
 
-cryptsetup luksOpen /dev/sda2 crypt
+cryptsetup luksOpen /dev/sda2 crypt # OR for software raid: cryptsetup luksOpen /dev/md1 crypt
 pvcreate /dev/mapper/crypt
 cp vg0.freespace /etc/lvm/backup/vg0
 
@@ -96,7 +102,7 @@ vgchange -a y vg0
 #Ok, the filesystem is missing, lets create it:
 mkfs.ext4 /dev/vg0/root
 mkfs.xfs /dev/vg0/backup
-mkfs.ext4 /dev/vg0/var
+mkfs.xfs /dev/vg0/var
 mkfs.ext4 /dev/vg0/log
 mkswap /dev/vg0/swap
 
@@ -106,7 +112,7 @@ mkdir -p /mnt/var/log /mnt/backup
 mount /dev/vg0/var /mnt/var/
 mount /dev/vg0/log /mnt/var/log
 mount /dev/vg0/backup /mnt/backup
-rsync -a /oldroot/ /mnt/
+rsync -av /oldroot/ /mnt/
 
 # Now ready for reboot
 umount /mnt/backup 
